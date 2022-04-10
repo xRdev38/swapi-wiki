4aec20a747f64099466188cfc2d87d70c8c9cd56
xRdev38 <developer.rudy@gmail.com>
2022-04-10 12:00:46 +0200

    chore: update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.(vue|js)", "!**/node_modules/**"],
+  coverageDirectory: "./coverage",
+  coverageReporters: ["html", "text", "text-summary"],
 };
